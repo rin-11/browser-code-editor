80ac8ccf23a9227874c4626882fdf746179b8f87
Rin Kates <erinkates11@gmail.com>
2023-11-14 13:28:27 -0500

    esbuild start service console working

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,7 +1,11 @@
+import * as esbuild from 'esbuild-wasm';
+
 import ReactDOM from "react-dom/client";
 
 // useState for the input in textarea from user and the output code
-import { useState } from 'react'; 
+// useEffect to startService
+import { useState, useEffect } from 'react'; 
+import { start } from 'repl';
 
 
 const el = document.getElementById("root");
@@ -11,6 +15,17 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL:'/esbuild.wasm'
+    });
+    console.log(service)
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input)
