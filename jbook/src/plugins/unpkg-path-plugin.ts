6dd3fb1667caae32bf7bd4440c2af6b399322ce9
Rin Kates <erinkates11@gmail.com>
2023-11-15 21:30:01 -0500

    unpkg code cleaned up for handling

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -6,33 +6,30 @@ const fileCache = localForage.createInstance({
     name: 'filecache'
 });
 
-
 export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+    
+    // Handle root file entry of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return {path: 'index.js', namespace: 'a'};
+      });
+
+    // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+            namespace: 'a',
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
+        };
+      })
+    
+    // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js'){
-            return { path: args.path, namespace: 'a' };
-        } 
-        // check for relative path
-        if (args.path.includes('./') || args.path.includes('../')) {
-            return {
-                namespace: 'a',
-                path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href,
-            };
-        }
         return {
             namespace: 'a',
             path: `https://unpkg.com/${args.path}`
         };
-        // else if (args.path === 'tiny-test-pkg'){
-        //     return { 
-        //         path: 'https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //         namespace: 'a',
-        //     };
-        // }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
