19bb3c1e5192083f175513ed9ec7ae1ef68f7f11
Rin Kates <erinkates11@gmail.com>
2023-11-15 23:27:00 -0500

    can load jsx and css files same time

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -3,53 +3,65 @@ import axios from 'axios';
 import localForage from 'localforage';
 
 const fileCache = localForage.createInstance({
-    name: 'filecache'
+  name: 'filecache',
 });
 
 export const fetchPlugin = (inputCode: string) => {
-    return {
-        name: 'fetch-plugin',
-        setup(build: esbuild.PluginBuild){
-        build.onLoad({ filter: /.*/ }, async (args: any) => {
-            console.log('onLoad', args);
-     
-            if (args.path === 'index.js') {
-              return {
-                loader: 'jsx',
-                contents: inputCode,
-              };
-            } 
-        
-            // Check to see if file has already been fetched (stored in the cache)
-            const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-            // if so, return stored file
-            if (cachedResult){
-                return cachedResult;
-            }
-            const { data, request } = await axios.get(args.path);
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
-            // check for CSS files 
-            const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-            const contents = fileType === 'css' ?
-                `
-                    const style = document.createElement('style');
-                    styler.innerText = 'body { background-color: "red" }';
-                    document.head.appendChild(style);
-                `   : data
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
-            const result: esbuild.OnLoadResult = {
-                loader: 'jsx',
-                contents,
-                resolveDir: new URL('./', request.responseURL).pathname
-            };
-                // Files are stored in key-value pair
+        if (cachedResult) {
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // Check to see if file has already been fetched (stored in the cache)
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+		// if so, return stored file
+        if (cachedResult) {
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+		// Files are stored in key-value pair
                     // key = args.path
                     // value = response/result object
 
-            // if not, store response in cache and return result
-            await fileCache.setItem(args.path, result);
-            return result;
-          });
-        }
-    };
+         // if not, store response in cache and return result
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
 };
