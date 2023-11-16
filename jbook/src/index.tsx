ce6d01fb447061a2c2a3084a20f4dd5eb840100e
Rin Kates <erinkates11@gmail.com>
2023-11-16 15:16:45 -0500

    coode being executed in iframe

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,12 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
 import ReactDOM from "react-dom/client";
 
+import { useState, useEffect, useRef } from 'react'; 
 // useState for the input in textarea from user and the output code
 // useEffect to startService
 // useRef to transform code
-import { useState, useEffect, useRef } from 'react'; 
-import { start } from 'repl';
 
 // PLUGINS
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
@@ -36,11 +34,7 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    // transform function the input
-    // const result = await ref.current.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015'
-    // });
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -53,30 +47,35 @@ const App = () => {
         // define process.env.NoDE_ENV as a string of production to handle bundling error
         'process.env.NODE_ENV': "'production'",
         // replace global with window to display some packages in the browser
-        global: 'window'
+        global: 'window',
       }
     })
-    // console.log(result);
-    setCode(result.outputFiles[0].text);
 
-    // eval to run code
-    // protect against error in code with try/catch
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+    setCode(result.outputFiles[0].text);
   };
+
+  const html = 
+    `
+      <script>
+        ${code}
+      </script>
+    `;
+
 
   return (
   <div>
-    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+    <textarea 
+      value={input} 
+      onChange={e => setInput(e.target.value)}>
+    </textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} />
   </div>
   );
 };
+
 
 root.render(<App />);
