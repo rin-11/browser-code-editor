90f5bf0ca1eb2702d288697009b7771e4c0b2568
Rin Kates <erinkates11@gmail.com>
2023-11-16 17:34:59 -0500

    styling monaco

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,21 +1,38 @@
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 
-const CodeEditor = () => {
-    return <MonacoEditor 
-        language= "javascript" 
-        theme="dark" 
-        height="500px"
-        options={{
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 22,
-            scrollBeyondLastLine: false,
-            automaticLayout: true,
-        }}
-        />   
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+  };
+
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      theme="dark"
+      language="javascript"
+      height="500px"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
