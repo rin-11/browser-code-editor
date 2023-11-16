81802a9da9eef7f0e1a73bceecce3f12bf0c1951
Rin Kates <erinkates11@gmail.com>
2023-11-16 17:17:20 -0500

    code editor styling

--- jbook/src/components/code-editor.tsx
+++ jbook/src/components/code-editor.tsx
@@ -1,7 +1,21 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-    return <MonacoEditor height="500px"/>   
+    return <MonacoEditor 
+        language= "javascript" 
+        theme="dark" 
+        height="500px"
+        options={{
+            wordWrap: 'on',
+            minimap: { enabled: false },
+            showUnused: false,
+            folding: false,
+            lineNumbersMinChars: 3,
+            fontSize: 22,
+            scrollBeyondLastLine: false,
+            automaticLayout: true,
+        }}
+        />   
 };
 
 export default CodeEditor;
