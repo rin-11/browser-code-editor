0b91d436513657bcd59a23c07e5226cf883d3a67
Rin Kates <erinkates11@gmail.com>
2023-11-17 16:24:13 -0500

    resizing component

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -11,12 +11,22 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
 
   useEffect(() => {
+    let timer: any;
     const listener = () => {
-      setInnerHeight(window.innerHeight);
-      setInnerWidth(window.innerWidth);
-    };
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+        setWidth(window.innerWidth * 0.75);
+      }
+    }, 100);
+  };
     window.addEventListener('resize', listener);
 
     return () => {
@@ -51,8 +61,11 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       minConstraints: [innerWidth * 0.2, Infinity],
       maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: innerWidth * 0.75,
-      resizeHandles: ['e'],
+      width,
+      resizeHandles: ['e'],      
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
