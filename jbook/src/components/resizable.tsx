7ec7437a1d940a24f09c3ffdc783b4700afdb7e7
Rin Kates <erinkates11@gmail.com>
2023-11-17 14:40:24 -0500

    resizing screen based on window size fixed

--- jbook/src/components/resizable.tsx
+++ jbook/src/components/resizable.tsx
@@ -1,38 +1,63 @@
 import './resizable.css';
+import { useEffect, useState } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
-import { useEffect } from 'react';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
-  children?: React.ReactNode;  
+  children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     const listener = () => {
-      console.log(window.innerWidth, window.innerHeight);
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
     };
     window.addEventListener('resize', listener);
+
     return () => {
       window.removeEventListener('resize', listener);
-    }
+    };
   }, []);
+
+  // Handling ResizeObserver loop error
+  useEffect(() => {
+    const resizeErrorListener = (e: ErrorEvent) => {
+      //   prettier-ignore
+      if (e.message ==="ResizeObserver loop completed with undelivered notifications.") {
+              const resizeObserverErrDiv = document.getElementById("webpack-dev-server-client-overlay-div");
+              const resizeObserverErr = document.getElementById("webpack-dev-server-client-overlay");
+              if (resizeObserverErr) {
+                resizeObserverErr.setAttribute("style", "display: none");
+              }
+              if (resizeObserverErrDiv) {
+                resizeObserverErrDiv.setAttribute("style", "display: none");
+              }
+            }
+    };
+    window.addEventListener("error", resizeErrorListener);
+ 
+    return () => window.removeEventListener("error", resizeErrorListener);
+  }, []);
+
 
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: innerWidth * 0.75,
       resizeHandles: ['e'],
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 24],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       height: 300,
       width: Infinity,
       resizeHandles: ['s'],
