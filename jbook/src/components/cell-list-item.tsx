8eec9a24e469899cea8909bbff6f9131c8162bc5
Rin Kates <erinkates11@gmail.com>
2023-11-21 15:33:43 -0500

    connecting cell docs

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -1,4 +1,10 @@
-const CellListItem: React.FC = () => {
-    return <div>Cell List Item</div>
+import { Cell } from '../state';
+
+interface CellListItemProps {
+    cell: Cell;
+  }
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+    return <div>{cell.id}</div>;
 };
 export default CellListItem;
--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,13 +1,10 @@
 import { useTypedSelector } from "../hooks/use-typed-selector";
-
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
-    useTypedSelector(({ cells: { order, data } }) => {
-            return order.map((id) => {
-                return data[id];
-            });
-    });
-    
-    return <div>Cell List</div>
+    const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />);
+
+    return <div>{renderedCells}</div>
 };
 export default CellList;
