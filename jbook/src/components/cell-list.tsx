aa840442916428252f11b7abc9a3048941457415
Rin Kates <erinkates11@gmail.com>
2023-11-21 15:16:41 -0500

    working on cell list, updated cell reducer to debug;

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,4 +1,13 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+
+
 const CellList: React.FC = () => {
+    useTypedSelector(({ cells: { order, data } }) => {
+            return order.map((id) => {
+                return data[id];
+            });
+    });
+    
     return <div>Cell List</div>
 };
 export default CellList;
