fd6b950fd06119e537febe2f1600010309de13d8
Rin Kates <erinkates11@gmail.com>
2023-11-21 21:17:49 -0500

    styling

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
@@ -7,15 +8,18 @@ const CellList: React.FC = () => {
     order.map((id) => data[id]));
 
     const renderedCells = cells.map((cell) => (
-        <>
+        <Fragment key={cell.id}>
         <AddCell nextCellId={cell.id} />
-    <CellListItem key={cell.id} cell={cell} />
-        </>
+        <CellListItem cell={cell} />
+        </Fragment>
    ));
 
-    return <div>
-        {renderedCells}
-        <AddCell nextCellId={null} />
+    return (
+        <div>
+            {renderedCells}
+            <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+
         </div>
+    );
 };
 export default CellList;
