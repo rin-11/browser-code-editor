3acbd01d6b6f702a35a60bc8d86d8f49b535d9c6
Rin Kates <erinkates11@gmail.com>
2023-11-25 14:49:13 -0500

    show() working in preview window

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -22,8 +22,20 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       `
+      import React from 'react';
+      import _ReactDOM from 'react-dom';
       const show = (value) => {
-        document.querySelector('#root').innerHTML = value;
+        const root = document.querySelector('#root');
+
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root);
+          } else {
+            root.innerHTML = JSON.stringify(value);
+          }
+        } else {
+          root.innerHTML = value;
+        }
       };
     `,
     ];
