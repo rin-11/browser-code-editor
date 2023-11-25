6ae736cbf9091b24012b546cfb6b3d61d53236e9
Rin Kates <erinkates11@gmail.com>
2023-11-25 15:10:22 -0500

    show function only to designated window

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -20,36 +20,41 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
 
-    const cumulativeCode = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-      const show = (value) => {
-        const root = document.querySelector('#root');
+    const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    var show = (value) => {
+      const root = document.querySelector('#root');
 
-        if (typeof value === 'object') {
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, root);
-          } else {
-            root.innerHTML = JSON.stringify(value);
-          }
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
         } else {
-          root.innerHTML = value;
+          root.innerHTML = JSON.stringify(value);
         }
-      };
-    `,
-    ];
-    for (let c of orderedCells){
-      if (c.type === 'code'){
-        cumulativeCode.push(c.content)
+      } else {
+        root.innerHTML = value;
       }
-      // stop when current code cell is reached
-      if (c.id === cell.id){
+    };
+  `;
+    const showFuncNoop = 'var show = () => {}';
+    const cumulativeCode = [];
+    for (let c of orderedCells) {
+      if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
+        cumulativeCode.push(c.content);
+      }
+      if (c.id === cell.id) {
         break;
       }
     }
     return cumulativeCode;
   });
+
 
 
   useEffect(() => {
