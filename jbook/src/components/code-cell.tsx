109560894e08f939430c0e10da973c071151c50d
Rin Kates <erinkates11@gmail.com>
2023-11-25 14:53:12 -0500

    name collision avoided with react import by updating jsx in es builder

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -22,7 +22,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       `
-      import React from 'react';
+      import _React from 'react';
       import _ReactDOM from 'react-dom';
       const show = (value) => {
         const root = document.querySelector('#root');
