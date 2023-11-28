1e51c267ededc00c0d8d558f403758352a364997
Rin Kates <erinkates11@gmail.com>
2023-11-28 17:31:36 -0500

    options working

--- jbook/packages/cli/dist/commands/serve.js
+++ jbook/packages/cli/dist/commands/serve.js
@@ -10,6 +10,6 @@ exports.serveCommand = new commander_1.Command()
     .option('-p, --port <number>', 'port to run server on', '4005')
     // option for user to determine which port to run the server on else default 4005
     /// <> indicate required value for port hence default provided
-    .action(() => {
-    console.log('Ready to serve');
+    .action((filename = 'notebook.js', options) => {
+    console.log(filename, options);
 });
--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -12,7 +12,7 @@ export const serveCommand = new Command()
     .option('-p, --port <number>', 'port to run server on', '4005')
     // option for user to determine which port to run the server on else default 4005
     /// <> indicate required value for port hence default provided
-    
-    .action(() => { 
-        console.log('Ready to serve')
+
+    .action((filename = 'notebook.js', options: { port: string }) => {
+        console.log(filename, options)
   });
