4e85df0f603ee8dd0734674c55a18a852153453f
Rin Kates <erinkates11@gmail.com>
2023-11-28 21:07:23 -0500

    production build connecting local api and local client

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
   const app = express();
 
-  // connecting to the proxy
-  app.use(createProxyMiddleware({
-    target: 'http://127.0.0.1:3000',
-    ws: true,
-    logLevel: 'silent',
-  }))
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
-  // error handling
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
